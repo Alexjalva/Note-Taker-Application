a9730288014de1a68f94c0843115b8f7bd3406cc
Alex Alvarado <Alexjalva@gmail.com>
2021-03-04 18:41:25 -0800

    trying to fix deploy

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,11 @@
 const fs = require('fs');
 const path = require('path');
 
-let notes;
-fs.readFile('/db/db.json', "utf8", (err,data) => {
-if(err){ throw err;}
+let notesArray;
+fs.readFile('db/db.json', "utf8", (err,data) => {
+if(err){
+     throw err;
+    }
 notesArray=JSON.parse(data);
 });
 
@@ -12,13 +14,13 @@ notesArray=JSON.parse(data);
 module.exports = (app) => {
  
 
-  app.get('/api/notes', (req, res) => res.json(notes));
+  app.get('/api/notes', (req, res) => res.json(notesArray));
 
   app.post('/api/notes', (req, res) => {
 
     let newNote = req.body;
     notesArray.push(newNote);
-    fs.writeFile('/db/db.json', JSON.stringify(notes), err => {
+    fs.writeFile('db/db.json', JSON.stringify(notesArray), err => {
         if(err) {
             throw err;
         }
